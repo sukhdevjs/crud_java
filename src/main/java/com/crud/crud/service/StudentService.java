4153042f8498e6b757c63e3804210534d48e7f47
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-03 11:01:58 +0545

    Updated StudentRepository, StudentService, and application.properties for functioning of api

--- src/main/java/com/crud/crud/service/StudentService.java
+++ src/main/java/com/crud/crud/service/StudentService.java
@@ -14,14 +14,17 @@ public class StudentService {
     private StudentRepository studentRepository;
 
     public List<Student> getAllStudents(){
+
         return studentRepository.findAll();
     }
 
     public Student getStudentById (Long id){
+
         return studentRepository.findById(id).orElse(null);
     }
 
     public Student createStudent (Student student){
+
         return studentRepository.save(student);
     }
 
